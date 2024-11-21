193ef93785ae1b23803ba45ed50d85feb915a5c6
Zagir <45565688+zagir-valitov@users.noreply.github.com>
2024-11-21 16:03:13 +0500

    Немного подредактировал

--- Query.sql
+++ Query.sql
@@ -17,43 +17,43 @@ CREATE TABLE Products
 
 CREATE TABLE Orders
 (
-		ID SERIAL PRIMARY KEY,
-		CustomerID INTEGER REFERENCES Customers(ID) ON DELETE CASCADE,
-		ProductID INTEGER REFERENCES Products(ID) ON DELETE CASCADE,
-		Quantity INTEGER		
+	ID SERIAL PRIMARY KEY,
+	CustomerID INTEGER REFERENCES Customers(ID) ON DELETE CASCADE,
+	ProductID INTEGER REFERENCES Products(ID) ON DELETE CASCADE,
+	Quantity INTEGER		
 );
 
 -- 4) -----------------------------------------------------------
 INSERT INTO Customers (FirstName, LastName, Age) 
 VALUES 
-('Anton', 'Antonov', 28),
-('Boris', 'Boriskin', 45),
-('Victoria', 'Vetlitskaya', 32),
-('Galina', 'Gerasimova', 24),
-('Dmitry', 'Davydov', 50),
-('Evgeny', 'Ezhgurov', 35),
-('Zhanna', 'Zhuravleva', 20),
-('Irina', 'Ignachkova', 37),
-('Klavdiya', 'Kosygina', 50),
-('Lyudmilla', 'Lyubimova', 40);
+	('Anton', 'Antonov', 28),
+	('Boris', 'Boriskin', 45),
+	('Victoria', 'Vetlitskaya', 32),
+	('Galina', 'Gerasimova', 24),
+	('Dmitry', 'Davydov', 50),
+	('Evgeny', 'Ezhgurov', 35),
+	('Zhanna', 'Zhuravleva', 20),
+	('Irina', 'Ignachkova', 37),
+	('Klavdiya', 'Kosygina', 50),
+	('Lyudmilla', 'Lyubimova', 40);
 
 INSERT INTO Products (Description, StockQuantity, Price)
 VALUES
-('Bread', 20, 35.55),
-('Butter', 50, 200.80),
-('Sugar', 80, 45.30),
-('Salt', 100, 15.25),
-('Eggs', 30, 60.99),
-('Milk', 60, 85.00),
-('Sausage', 30, 350.99),
-('Fish', 15, 200.85),
-('Tomato', 200, 40.50),
-('Apple', 300, 65.00);
+	('Bread', 20, 35.55),
+	('Butter', 50, 200.80),
+	('Sugar', 80, 45.30),
+	('Salt', 100, 15.25),
+	('Eggs', 30, 60.99),
+	('Milk', 60, 85.00),
+	('Sausage', 30, 350.99),
+	('Fish', 15, 200.85),
+	('Tomato', 200, 40.50),
+	('Apple', 300, 65.00);
 
 INSERT INTO Orders (CustomerID, ProductID, Quantity)
 VALUES
-(1, 10, 1), (2, 1, 5), (3, 8, 3), (4, 1, 10), (5, 6, 8),
-(6, 1, 7), (7, 4, 2), (8, 1, 6), (9, 2, 11), (10, 1, 9);
+	(1, 10, 1), (2, 1, 5), (3, 8, 3), (4, 1, 10), (5, 6, 8),
+	(6, 1, 7), (7, 4, 2), (8, 1, 6), (9, 2, 11), (10, 1, 9);
 
 --5) -----------------------------------------------------------
 SELECT
